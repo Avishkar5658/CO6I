755a6c2b34cc63d22b098928a02c0a0460352625
Avishkar5658 <117887629+Avishkar5658@users.noreply.github.com>
2023-05-10 07:48:23 +0530

    Add files via upload

---
+++ ExternalPr.py
@@ -0,0 +1,16 @@
+# write a python program to demonstrate string built in function.
+
+
+text = input("Enter thr string: ")
+
+print("\nConverted String:", text.upper())
+
+print("\nConverted String:", text.lower())
+
+print("\nConverted String:", text.title())
+
+print("\nConverted String:", text.swapcase())
+
+print("\nConverted String:", text.capitalize())
+
+print("\nOriginal String:", text)
